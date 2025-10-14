86955a3cf34dba1013291fa11364e4a25511716c
FellipeexHU3 <fellipeex@hotmail.com>
2025-10-14 17:40:28 -0300

    fix-ponto-gravar

--- modules/ponto/modulo_ponto.py
+++ modules/ponto/modulo_ponto.py
@@ -51,6 +51,8 @@ def bater_ponto(horario):
         pyautogui.click(1080, 375)
         time.sleep(0.5)
         pyautogui.click(171, 420) # salvar
+        time.sleep(0.5)
+        pyautogui.press('enter')
         print("🎉 Ponto registrado com sucesso!")
 
     except Exception as e:
